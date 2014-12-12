368f1ebe6d47460ac2762a5f122258b46b071984
Issa Rice <riceissa@gmail.com>
2014-12-11 17:17:49 -0800

    work on streamlining conversion

--- generate.py
+++ generate.py
@@ -1,19 +1,36 @@
 #!/bin/python
 
 import glob
-from subprocess import call
+from subprocess import call, check_output
 import os
+import meta
+import json
 
 lst_filepaths = glob.glob("pages/*.md")
-lst_filenames = [os.path.basename(i) for i in lst_filepaths]
-lst_routenames = [os.path.splitext(i)[0] for i in lst_filenames]
+#lst_filenames = [os.path.basename(i) for i in lst_filepaths]
+#lst_routenames = [os.path.splitext(i)[0] for i in lst_filenames]
 
-def generate_html():
-    for n, filepath in enumerate(lst_filepaths):
-        command = "pandoc -f markdown -t html -s --base-header-level=2 -o _site/{routename} {filepath}".format(filepath=filepath, routename=lst_routenames[n])
+def markdown_to_json(filepath):
+    command = "pandoc -f markdown -t json -s {filepath}".format(filepath=filepath)
+    return check_output(command, shell=True)
+
+def convert_single_file(filepath, outdir="_site/"):
+    if os.path.exists(outdir):
+        filename = os.path.basename(filepath)
+        routename = os.path.splitext(filename)
+        command = "pandoc -f markdown -t html -s --base-header-level=2 -o {outdir}{routename} {filepath}".format(filepath=filepath, routename=routename, outdir=outdir)
         print command
         #call(command, shell=True)
+    else:
+        print("{outdir} does not exist!".format(outdir=outdir))
 
-if os.path.exists("_site/"):
-    generate_html()
+def generate_html():
+    for filepath in lst_filepaths:
+        convert_single_file(filepath)
+
+#myjson = json.loads(markdown_to_json("hello.md"))
+#print myjson
+#meta.organize_tags(myjson, meta.tag_synonyms, meta.tag_implications)
+
+generate_html()
 
