d0d826a9e48c9b93af54aea7aa98eade328980be
Issa Rice <riceissa@gmail.com>
2014-12-11 21:39:11 -0800

    page of tags + page of pages for each tag work

--- generate.py
+++ generate.py
@@ -24,7 +24,7 @@ def convert_single_file(filepath, outdir="_site/"):
         myjson = json.loads(markdown_to_json(filepath))
         myjson = meta.organize_tags(myjson, meta.tag_synonyms, meta.tag_implications)
         #print myjson['tags']
-        all_tags.extend(myjson['tags'])
+        all_tags[routename] = myjson['tags']
         myjson = myjson['json_dump']
         command = "pandoc -f json -t html -s --template=skeleton.html --base-header-level=2 -o {outdir}{routename}".format(routename=routename, outdir=outdir)
         print command
@@ -41,10 +41,18 @@ def generate_html():
     for filepath in lst_filepaths:
         convert_single_file(filepath, outdir="_site/")
 
-def create_tag_pages(tags):
+def create_tag_pages(tags_dict):
+    tags = list(set([val for subl in tags_dict.values() for val in subl]))
+    print tags
+    print tags_dict
     for tag in tags:
-        template = Template("""Tag: {{ tag }}""")
-        doc = template.render(tag=tag, seq=tags)
+        pages_with_tag = [page for page, page_tags in tags_dict.items() if tag in page_tags]
+        print str(pages_with_tag) + " has " + tag
+        template = Template("""Tag: {{ tag }}
+
+{% for page in pages_with_tag %}- [{{ page }}](./{{ page }})
+{% endfor %}""")
+        doc = template.render(tag=tag, pages_with_tag=pages_with_tag)
         command = 'pandoc -V title="Tag: {tag}" -f markdown -t html --template=skeleton.html -o _site/tags/{tag}'.format(tag=tag)
         ps = Popen(shlex.split(command), stdout=PIPE, stdin=PIPE, stderr=STDOUT)
         output = ps.communicate(input=doc)[0]
@@ -59,11 +67,11 @@ def create_all_tags_page(tags):
         ps = Popen(shlex.split(command), stdout=PIPE, stdin=PIPE, stderr=STDOUT)
         output = ps.communicate(input=doc)[0]
 
-all_tags = []
+all_tags = {}
 #generate_html()
-#print list(set(all_tags))
-lst = ["a", "b", "hakyll"]
-create_tag_pages(lst)
-create_all_tags_page(lst)
+print list(set(all_tags))
+d = {"article1": ["a", "b"], "article2": ["b", "hakyll"]}
+create_tag_pages(d)
+create_all_tags_page(list(set([i for subl in d.values() for i in subl])))
 
 
