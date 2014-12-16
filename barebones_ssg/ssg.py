e112eca77933129a1ddfd886ef82799fecc842c5
Issa Rice <riceissa@gmail.com>
2014-12-16 03:21:54 -0800

    fix encode decode issues

--- barebones_ssg/ssg.py
+++ barebones_ssg/ssg.py
@@ -1,18 +1,15 @@
-
-# hack to get unicode working with jinja2
-#import sys
-#reload(sys)
-#sys.setdefaultencoding('utf-8')
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import glob
-import metadata as meta
-from tag_ontology import *
-import commands as c
 import json
 from collections import OrderedDict
-
 from jinja2 import Template, Environment, FileSystemLoader
 import os
+
+import commands as c
+import metadata as meta
+from tag_ontology import *
 
 def to_unicode(string):
     if isinstance(string, str):
@@ -51,28 +48,19 @@ for page in pages_lst:
     json_lst = json.loads(output)
     file_dict = meta.organize_tags(json_lst, tag_synonyms, tag_implications)
     tags_lst = meta.get_tags(file_dict['json'])
-    tags_lst = [i.decode('utf-8') for i in tags_lst]
+    tags_lst = [to_unicode(i) for i in tags_lst]
     all_tags.extend(tags_lst)
     json_str = json.dumps(file_dict['json'], separators=(',',':'))
     body = c.run_command("pandoc -f json -t html --toc --template=templates/toc.html --mathjax --base-header-level=2", pipe_in=json_str).decode('utf-8')
-    title = meta.get_metadata_field(json_lst, "title")
-    #print "TITLETYPE0:", type(title)
-    title = title.decode('utf-8') if not isinstance(title, unicode) else title
-    math = meta.get_metadata_field(json_lst, "math")
-    if isinstance(math, bool):
-        if math:
-            math = "True"
-        else:
-            math = "False"
-    math = math.decode('utf-8') if not isinstance(math, type(None)) else unicode('', 'utf-8')
-    license = meta.get_metadata_field(json_lst, "license")
-    license = license.decode('utf-8') if not isinstance(license, type(None)) else unicode('', 'utf-8')
+    title = to_unicode(meta.get_metadata_field(json_lst, "title"))
+    math = to_unicode(meta.get_metadata_field(json_lst, "math"))
+    license = to_unicode(meta.get_metadata_field(json_lst, "license"))
 
     env = Environment(loader=FileSystemLoader('.'))
     skeleton = env.get_template('templates/skeleton.html')
     tags = []
     for tag in tags_lst:
-        tags.append({'name': tag.decode('utf-8'), 'path': ("tags/" + tag).decode('utf-8')})
+        tags.append({'name': to_unicode(tag), 'path': to_unicode("tags/" + tag)})
     tags = sorted(tags, key=lambda t: t['name'])
     final = skeleton.render(body=body, title=title, license=license, math=math, tags=tags)
     inter = os.path.split(os.path.splitext(page)[0])[1]
@@ -80,7 +68,7 @@ for page in pages_lst:
     page_data.append((title, inter, tags_lst))
 
     with open(write_to, 'w') as f:
-        f.write(final.encode('utf-8'))
+        f.write(to_string(final))
 
 all_tags = list(set(all_tags))
 
@@ -88,40 +76,34 @@ for tag in all_tags:
     pages = []
     for page_tuple in page_data:
         if tag in page_tuple[2]:
-            pages.append({'title': page_tuple[0], 'url': "../" + page_tuple[1]})
+            pages.append({'title': to_unicode(page_tuple[0]), 'url': to_unicode("../" + page_tuple[1])})
     pages = sorted(pages, key=lambda t: t['title'])
-    write_to = "_site/tags/" + tag.encode('utf-8')
+    write_to = "_site/tags/" + to_string(tag)
     env = Environment(loader=FileSystemLoader('.'))
     page_list = env.get_template('templates/page-list.html')
-    body = page_list.render(pages=pages)
+    body = to_unicode(page_list.render(pages=pages))
     skeleton = env.get_template('templates/skeleton.html')
-    final = skeleton.render(body=body, title="Tag page for " + tag)
+    final = skeleton.render(body=body, title=to_unicode("Tag page for " + tag))
 
     with open(write_to, 'w') as f:
-        f.write(final.encode('utf-8'))
+        f.write(to_string(final))
 
 # Make page with all tags
 env = Environment(loader=FileSystemLoader('.'))
 page_list = env.get_template('templates/page-list.html')
-pages = [{'title': tag.decode('utf-8'), 'url': tag.decode('utf-8')} for tag in all_tags]
+pages = [{'title': to_unicode(tag), 'url': to_unicode(tag)} for tag in all_tags]
 pages = sorted(pages, key=lambda t: t['title'])
-body = page_list.render(pages=pages).decode('utf-8')
+body = to_unicode(page_list.render(pages=pages))
 skeleton = env.get_template('templates/skeleton.html')
-final = skeleton.render(title="All tags".decode('utf-8'), body=body)
+final = skeleton.render(title=to_unicode("All tags"), body=body)
 with open("_site/tags/index", 'w') as f:
-    f.write(final.encode('utf-8'))
+    f.write(to_string(final))
 
 # Make page with all pages
 env = Environment(loader=FileSystemLoader('.'))
 page_list = env.get_template('templates/page-list.html')
-#print "len(page_list)", len(page_list)
 pages = [{'title': to_unicode(page_tup[0]), 'url': to_unicode(page_tup[1])} for page_tup in page_data]
 pages = sorted(pages, key=lambda t: t['title'])
-#for i in pages:
-    #print i['title']
-    #print i['url']
-    #print ""
-#print "len(pages)", len(pages)
 body = page_list.render(pages=pages)
 skeleton = env.get_template('templates/skeleton.html')
 final = skeleton.render(title=to_unicode("All pages on the site"), body=body)
